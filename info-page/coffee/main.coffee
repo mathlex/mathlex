443907c3dc0317dc05f70fbaf6f436dd943af835
Matthew Barry <komputerwiz.matt@gmail.com>
2012-12-17 12:45:42 -0600

    Light and Dark themes

--- info-page/coffee/main.coffee
+++ info-page/coffee/main.coffee
@@ -1,9 +1,24 @@
+$('#power-button').on 'click', (evt) ->
+  evt.preventDefault()
+  if localStorage.theme is 'dark'
+    localStorage.theme = 'light'
+    $('body').removeClass('dark').addClass('light');
+  else
+    localStorage.theme = 'dark'
+    $('body').removeClass('light').addClass('dark');
+
+if localStorage.theme? and localStorage.theme is 'light'
+  $('body').addClass('light').removeClass('dark');
+else
+  $('body').addClass('dark').removeClass('light');
+
 path = (args...) ->
   arg.replace '@', 'javascripts/lib/syntaxhighlighter/scripts/' for arg in args
 
 SyntaxHighlighter.autoloader.apply null, path(
   'html @shBrushXml.js'
   'js @shBrushJScript.js'
+  'shell @shBrushBash.js'
 )
 
 SyntaxHighlighter.all()
--- info-page/javascripts/main.js
+++ info-page/javascripts/main.js
@@ -3,6 +3,23 @@
   var path,
     __slice = [].slice;
 
+  $('#power-button').on('click', function(evt) {
+    evt.preventDefault();
+    if (localStorage.theme === 'dark') {
+      localStorage.theme = 'light';
+      return $('body').removeClass('dark').addClass('light');
+    } else {
+      localStorage.theme = 'dark';
+      return $('body').removeClass('light').addClass('dark');
+    }
+  });
+
+  if ((localStorage.theme != null) && localStorage.theme === 'light') {
+    $('body').addClass('light').removeClass('dark');
+  } else {
+    $('body').addClass('dark').removeClass('light');
+  }
+
   path = function() {
     var arg, args, _i, _len, _results;
     args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
@@ -14,7 +31,7 @@
     return _results;
   };
 
-  SyntaxHighlighter.autoloader.apply(null, path('html @shBrushXml.js', 'js @shBrushJScript.js'));
+  SyntaxHighlighter.autoloader.apply(null, path('html @shBrushXml.js', 'js @shBrushJScript.js', 'shell @shBrushBash.js'));
 
   SyntaxHighlighter.all();
 
